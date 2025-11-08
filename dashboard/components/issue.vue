6af15f693a789425ff3b8a5e1b20ee9ff04f0207
SamuelCooper1105 <coopercumens@gmail.com>
2025-11-08 11:34:44 -0500

    dashboard + pocketgo

--- dashboard/components/issue.vue
+++ dashboard/components/issue.vue
@@ -2,6 +2,16 @@ import PocketBase from 'pocketbase';
 
 const pb = new PocketBase('https://127.0.0.1:8090');
 
-const record = await pb.collection('reports').getOne('RECROD_ID', 
+const record = await pb.collection('reports').getOne('RECORD_ID', 
 {expand:'relField1,relField2.subRelField',});
 
+const email = record.email;
+const url - record.url;
+
+
+return (
+    <div>
+        <p>Email: {record.email}</p>
+        <p>URL: <a href={record.url}</a></p>
+    </div>
+);
