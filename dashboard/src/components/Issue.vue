4d7af6b0c6fc737f20febc5cb10ef7d9e9d031b6
SamuelCooper1105 <coopercumens@gmail.com>
2025-11-08 12:05:11 -0500

    changed Issue.vue

---
+++ dashboard/src/components/Issue.vue
@@ -0,0 +1,17 @@
+<script setup>
+    const props = defineProps({
+        email: String,
+        URL: String,
+        issue: String,
+        severity: String
+
+    })
+</script>
+
+
+<template>
+        <p>Email: {{props.email}}</p>
+        <p>URL: <a :href="props.url">{{ props.URL }}</a></p>
+        <p>Issue: {{props.issue}}</p>
+        <p>Severity: {{props.severity}}</p>
+</template>
