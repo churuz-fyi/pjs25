99a5f8bd9da27b31ab84de803015a9cc9007a00f
SamuelCooper1105 <coopercumens@gmail.com>
2025-11-08 11:37:38 -0500

    issue added

--- dashboard/components/issue.vue
+++ dashboard/components/issue.vue
@@ -6,12 +6,14 @@ const record = await pb.collection('reports').getOne('RECORD_ID',
 {expand:'relField1,relField2.subRelField',});
 
 const email = record.email;
-const url - record.url;
+const url = record.url;
+const issue = record.issue;
 
 
 return (
     <div>
         <p>Email: {record.email}</p>
         <p>URL: <a href={record.url}</a></p>
+        <p>Issue: {record.issue}</p>
     </div>
 );
