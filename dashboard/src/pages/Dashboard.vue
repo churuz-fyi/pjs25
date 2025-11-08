5e7375d8f131b22fbde4b3f9d450017d5de7e4fe
SamuelCooper1105 <coopercumens@gmail.com>
2025-11-08 14:25:28 -0500

    stringify

--- dashboard/src/pages/Dashboard.vue
+++ dashboard/src/pages/Dashboard.vue
@@ -1,11 +1,15 @@
 <script setup>
-      import PocketBase from 'pocketbase';
+    import PocketBase from 'pocketbase';
     import Issue from '../components/Issue.vue'
     const pb = new PocketBase('http://127.0.0.1:8090');
     await pb.collection("_superusers").authWithPassword('ruizramos04@gmail.com','awesome123');
     const results = await pb.collection('reports').getFullList();
     console.log(results);
+    
+    const response = JSON.stringify(results);
+
 </script>
+
 <template>
       <div v-for="result in results.items" :key="result.id">{{result.collectionName}}</div>
       <Issue
