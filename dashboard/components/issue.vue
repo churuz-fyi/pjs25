4b5eb18346efd41bd916d46b7a8a604fa70d9731
SamuelCooper1105 <coopercumens@gmail.com>
2025-11-08 11:25:23 -0500

    issue

---
+++ dashboard/components/issue.vue
@@ -0,0 +1,7 @@
+import PocketBase from 'pocketbase';
+
+const pb = new PocketBase('https://127.0.0.1:8090');
+
+const record = await pb.collection('reports').getOne('RECROD_ID', 
+{expand:'relField1,relField2.subRelField',});
+
